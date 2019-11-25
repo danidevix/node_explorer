4d2e1a48c5f9ee5cf818113b22c94b3ba7957ec7
danidevix <31804080+danidevix@users.noreply.github.com>
2019-11-25 12:43:08 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+useradd -M -r -s /bin/false node_exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v0.18.0/node_exporter-0.18.0.linux-amd64.tar.gz
+tar xzf node_exporter-0.18.0.linux-amd64.tar.gz
+cp node_exporter-0.18.0.linux-amd64/node_exporter /usr/local/bin/
+chown node_exporter:node_exporter /usr/local/bin/node_exporter
+cp node_exporter.service /etc/systemd/system/node_exporter.service
+systemctl daemon-reload
+systemctl start node_exporter.service
+systemctl enable node_exporter.service
