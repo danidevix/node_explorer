457583dafd2a6efa30092c69523dcf928da2412b
danidevix <31804080+danidevix@users.noreply.github.com>
2020-04-06 11:45:38 +0700

    Create prometheus.sh
    chmod +x prometheus.sh
    ./prometheus.sh

---
+++ prometheus.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo adduser --no-create-home --disabled-login --shell /bin/false --gecos "Prometheus Monitoring User" prometheus
+sudo mkdir /etc/prometheus
+sudo mkdir /var/lib/prometheus
+sudo touch /etc/prometheus/prometheus.yml
+sudo touch /etc/prometheus/prometheus.rules.yml
+sudo chown -R prometheus:prometheus /etc/prometheus
+sudo chown prometheus:prometheus /var/lib/prometheus
+#VERSION=2.2.1
+VERSION=$(curl https://raw.githubusercontent.com/prometheus/prometheus/master/VERSION)
+wget https://github.com/prometheus/prometheus/releases/download/v${VERSION}/prometheus-${VERSION}.linux-amd64.tar.gz
+tar xvzf prometheus-${VERSION}.linux-amd64.tar.gz
+sudo cp prometheus-${VERSION}.linux-amd64/prometheus /usr/local/bin/
+sudo cp prometheus-${VERSION}.linux-amd64/promtool /usr/local/bin/
+sudo cp -r prometheus-${VERSION}.linux-amd64/consoles /etc/prometheus
+sudo cp -r prometheus-${VERSION}.linux-amd64/console_libraries /etc/prometheus
+sudo chown -R prometheus:prometheus /etc/prometheus/consoles
+sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
+sudo chown prometheus:prometheus /usr/local/bin/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/promtool
+cat ./prometheus/prometheus.yml | sudo tee /etc/prometheus/prometheus.yml
+cat ./prometheus/prometheus.rules.yml | sudo tee /etc/prometheus/prometheus.rules.yml
+cat ./prometheus/prometheus.service | sudo tee /etc/systemd/system/prometheus.service
+sudo systemctl daemon-reload
+sudo systemctl enable prometheus
+sudo systemctl start prometheus
+rm prometheus-${VERSION}.linux-amd64.tar.gz
+rm -rf prometheus-${VERSION}.linux-amd64
