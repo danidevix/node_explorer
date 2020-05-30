965f3b0b87d17d03353cd99588d1293f22cabb92
danidevix <31804080+danidevix@users.noreply.github.com>
2020-05-30 14:46:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ useradd -M -r -s /bin/false node_exporter
 wget isantri.moco.co.id/node_exporter-0.18.0.linux-amd64.tar.gz
 wget https://raw.githubusercontent.com/danidevix/node_explorer/master/node_exporter.service
 tar xzf node_exporter-0.18.0.linux-amd64.tar.gz
+yum install -y nano htop
 cp node_exporter-0.18.0.linux-amd64/node_exporter /usr/local/bin/
 chown node_exporter:node_exporter /usr/local/bin/node_exporter
 cp node_exporter.service /etc/systemd/system/node_exporter.service
