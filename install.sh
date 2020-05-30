c570bc789265863dd091e72e09e4e4a8f641c0e1
danidevix <31804080+danidevix@users.noreply.github.com>
2020-05-30 11:54:39 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 useradd -M -r -s /bin/false node_exporter
-yum install wget -y
 wget https://github.com/prometheus/node_exporter/releases/download/v0.18.0/node_exporter-0.18.0.linux-amd64.tar.gz
+wget https://raw.githubusercontent.com/danidevix/node_explorer/master/node_exporter.service
 tar xzf node_exporter-0.18.0.linux-amd64.tar.gz
 cp node_exporter-0.18.0.linux-amd64/node_exporter /usr/local/bin/
 chown node_exporter:node_exporter /usr/local/bin/node_exporter
