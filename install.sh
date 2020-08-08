33180d1598bd5794981f32fec0a219c1bde8d78e
danidevix <31804080+danidevix@users.noreply.github.com>
2020-08-08 19:32:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,6 @@ rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64 no
 #firewalld
 firewall-cmd --permanent --zone=public --add-rich-rule='
   rule family="ipv4"
-  source address="0.0.0.0"
+  source address="0.0.0.0/24"
   port protocol="tcp" port="9100" accept'
   firewall-cmd --reload
